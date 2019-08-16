d1b9c0e448b6bc40039a7c24beb6cc3f0383f995
Fredelyn Veraque <fredelynveraque@Fredelyns-MacBook-Pro.local>
2019-08-16 11:30:24 +0800

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,3 +52,15 @@ export default {
     }
   }
 }
+
+/* nuxt.config.js */
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/fredelynveraque/'
+  }
+} : {}
+
+export default {
+  ...routerBase
+}
